324963696e91ed4a0bf10a8fbb61a7357dcb33c0
Pippow <48692951+Pippow@users.noreply.github.com>
2026-06-10 12:12:23 +0200

    fixed all bugs with red riders (#798)

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Monsters/Copper/RedRider.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Monsters/Copper/RedRider.cs
@@ -5,10 +5,9 @@ using Alsein.Extensions;
 namespace Cynthia.Card
 {
     [CardEffectId("70083")]//红骑士
-    public class RedRider : CardEffect, IHandlesEvent<BeforeCardToCemetery>, IHandlesEvent<AfterCardToCemetery>, IHandlesEvent<AfterTurnOver>
+    public class RedRider : CardEffect, IHandlesEvent<BeforeCardToCemetery>
     {//每当有位于"刺骨冰霜"之下的敌军单位被摧毁时，从牌组召唤1张它的同名牌。
         public RedRider(GameCard card) : base(card) { }
-        public bool IsToSummon = false;
         public async Task HandleEvent(BeforeCardToCemetery @event)
         {
             if (@event.Target.Status.Type == CardType.Unit && @event.Target.PlayerIndex == AnotherPlayer && Card.Status.CardRow.IsInDeck())
@@ -21,30 +20,18 @@ namespace Cynthia.Card
                 bool isInFrost = Game.GameRowEffect[@event.Target.PlayerIndex][@event.DeathLocation.RowPosition.MyRowToIndex()].RowStatus == RowStatus.BitingFrost;
                 if (isInFrost)
                 {
-                    var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.Status.CardId == Card.Status.CardId && (x.Effect as RedRider)?.IsToSummon == false).ToList();
+                    var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.Status.CardId == Card.Status.CardId).ToList();
                     if (list.Count() == 0)
                     {
                         return;
                     }
                     //只召唤最后一个
-                    if (Card == list.First())
+                    if (Card == list.Last())
                     {
-                        IsToSummon = true;
+                        await Card.Effect.Summon(Game.GetRandomCanPlayLocation(Card.PlayerIndex, true), list.First());
                     }
                 }
             }
-        }
-        public async Task HandleEvent(AfterCardToCemetery @event)
-        {
-            if (IsToSummon)
-            {
-                IsToSummon = false;
-                await Card.Effect.Summon(Game.GetRandomCanPlayLocation(Card.PlayerIndex, true), Card);
-            }
-        }
-        public async Task HandleEvent(AfterTurnOver @event)
-        {
-           IsToSummon = false; 
         }
 
     }
